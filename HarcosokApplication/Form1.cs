0d76bc47d82b30ffe8fab1a9444078e65ba01d29
Petrik Lajos Szg <info@petrik.hu>
2018-12-14 12:42:33 +0100

    Alkalmazás indulásakor megnyitja az adatbázis kapcsolatot, és ha nincs adatbázis, a program azonnal leáll. A program bezárásakor bezárja az adatbázis kapcsolatot

--- HarcosokApplication/Form1.cs
+++ HarcosokApplication/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace HarcosokApplication
 {
     public partial class Form1 : Form
     {
+        MySqlConnection conn;
+
         public Form1()
         {
             InitializeComponent();
+            Databaseconn();
+
+
+
+
+
+
+            FormClosed += (sender, e) => Databaseclose();
         }
+
+        public void Databaseconn()
+        {
+            try {
+                conn = new MySqlConnection("Server=localhost;Database=cs_harcosok;Uid=root;Password=;");
+                conn.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Nincs megfelelő adatbázis! A program leáll!");
+                this.Close();
+            }
+            
+        }
+
+        public void Databaseclose()
+        {
+            conn = new MySqlConnection("Server=localhost;Database=cs_harcosok;Uid=root;Password=;");
+            conn.Close();
+        }
+
     }
 }
