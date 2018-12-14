067e6aa9341de9f1426fbc216db7ee388c650f18
Petrik Lajos Szg <info@petrik.hu>
2018-12-14 14:44:00 +0100

    Create table elkészült a programhoz, ha van adatbázis.

--- HarcosokApplication/Form1.cs
+++ HarcosokApplication/Form1.cs
@@ -19,7 +19,7 @@ namespace HarcosokApplication
         {
             InitializeComponent();
             Databaseconn();
-
+            Tablecreate();
 
 
 
@@ -37,16 +37,43 @@ namespace HarcosokApplication
             catch
             {
                 MessageBox.Show("Nincs megfelelő adatbázis! A program leáll!");
-                this.Close();
+                throw;
+                conn.Close();
             }
             
         }
-
         public void Databaseclose()
         {
             conn = new MySqlConnection("Server=localhost;Database=cs_harcosok;Uid=root;Password=;");
             conn.Close();
         }
+        public void Tablecreate()
+        {
+            var command1 = conn.CreateCommand();
+            command1.CommandText = @"
+            CREATE TABLE IF NOT EXISTS harcosok (
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            nev VARCHAR(255) UNIQUE,
+            letrehozas DATE NOT NULL          
+            );";
+            command1.ExecuteNonQuery();
+            var command2 = conn.CreateCommand();
+            command2.CommandText = @"
+            CREATE TABLE IF NOT EXISTS kepessegek (
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            nev TEXT ,
+            leiras TEXT ,
+            harcos_id INTEGER
+            );";
+            command2.ExecuteNonQuery();
+            var command3 = conn.CreateCommand();
+            command3.CommandText = @"
+            ALTER TABLE kepessegek
+            ADD FOREIGN KEY (harcos_id) REFERENCES harcosok(id);
+            ";
+            command3.ExecuteNonQuery();
+        }
+
 
     }
 }
